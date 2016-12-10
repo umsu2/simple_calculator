e45fd5c07ed4ac14f27daecff3d68f6a1cf6be12
Yang Su <yangsu.personal@gmail.com>
2016-12-10 16:46:21 -0600

    made a simple gui that takes two numbers and an operator and contact the backend for the result

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,4 @@
+import aiohttp_cors as aiohttp_cors
 from aiohttp import web
 import uvloop
 import asyncio
@@ -8,8 +9,44 @@ async def hello(request):
     data = {'hello': 'you devilish data'}
     return web.json_response(data=data)
 
+async def calculate(request):
+
+    data = await request.post()
+    data= dict(data)
+    val1 = int(data['var1'])
+    val2 = int(data['var2'])
+    result = {'result': operator_mapping(data['operator'])(val1,val2)}
+    return web.json_response(data = result)
+
+def operator_mapping(op):
+
+    operators = {
+        '/': int.__truediv__,
+        'mod': int.__mod__,
+        'x': int.__mul__,
+        '+': int.__add__,
+        '-': int.__sub__
+    }
+    return operators[op]
 
 app = web.Application()
 app.router.add_get('/', hello)
+app.router.add_post('/',calculate)
 
+def allow_all_cors():
+
+    # Configure default CORS settings.
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*",
+            )
+    })
+
+    # Configure CORS on all routes.
+    for route in list(app.router.routes()):
+        cors.add(route)
+
+allow_all_cors()
 web.run_app(app)
---
+++ frontend/calculator.js
@@ -0,0 +1,25 @@
+/**
+ * Created by yang on 10/12/16.
+ */
+
+$(document).ready(function() {
+    $('#calculate_form').submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            url: 'http://localhost:8080/',
+            type: 'POST',
+            data: $('#calculate_form').serialize(),
+            success: function (event) {
+
+                console.log(event);
+
+                $('#calc_result').text('Result is: '+ event.result);
+                console.log('form submitted.');
+
+            }
+        });
+        return false;
+    });
+});
+
+// action="http://localhost:8080/" method="post"
